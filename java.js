d757c17b8092ce414eed4d01dc31bc6c467db270
mdrobiulislamo9 <robiul.rubin@gmail.com>
2022-08-22 01:31:35 +0600

    50% done

--- java.js
+++ java.js
@@ -1,7 +1,10 @@
 document.getElementById('afif').addEventListener('click', function(){
     const name = document.getElementById('afif-hossain') ;
     const list = document.getElementById('list') ;
-    const olList = list.innerHTML = `<li></Li>` ;
-    list.appendChild(olList)
-    console.log(list) ;
+    const li = document.createElement('li') ;
+    li.innerText = name.innerText ;
+    list.appendChild(li) ;
+    const button = document.getElementById('afif') ;
+    button.disabled = true ;
+    
 })
